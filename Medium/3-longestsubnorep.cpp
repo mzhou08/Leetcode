59cce0023e0f48946e2da26a92d6bb6751ad962f
Michael <michael@Admins-MacBook-Pro.local>
2021-10-18 14:44:27 -0400

    3-longest substring with no repeats finished

--- Medium/3-longestsubnorep.cpp
+++ Medium/3-longestsubnorep.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+/*
+Submission Statistics:
+Runtime: 132 ms, faster than 11.94% of C++ online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 104.4 MB, less than 9.22% of C++ online submissions for Longest Substring Without Repeating Characters.
+*/
+
 class Solution {
 public:
     struct dict {
@@ -13,12 +19,12 @@ public:
         dict(char x, int ind) : val(x), index(ind) {}
     };
 
-    vector<int> repeat(vector<dict*> d, char c) {
+    vector<int> repeat(vector<dict*> d, char c, int start, int end) {
         vector<int> res;
         res.push_back(-1 /*index of dict*/);
         res.push_back(-1 /*index of repeat*/);
         for (int i = 0; i < d.size(); i++) {
-            if (d[i]->val == c) {
+            if (d[i]->val == c && d[i]->index >= start) {
                 res[0] = i;
                 res[1] = d[i]->index;
                 return res;
@@ -34,14 +40,17 @@ public:
         vector<dict*> d;
 
         while (end < s.length()) {
-            if (repeat(d, s[end])[0] != -1) {
+            if (repeat(d, s[end], start, end)[0] != -1) {
+                vector<int> rep = repeat(d, s[end], start, end);
+                //cout << rep[0] << " " << rep[1] << '\n';
+                
                 longest = max(longest, end - start);
-                start = repeat(d, s[end])[1] + 1;
+                start = rep[1] + 1;
 
                 //Update the dictionary entry for the char at s[end]
                 dict* new_entry = new dict(s[end], end);
-                d[repeat(d, s[end])[0]] = new_entry;
-                //cout << repeat(d, s[end]) << '\n';
+                d[rep[0]] = new_entry;
+                
             } else if (end == s.length() - 1) {
                 longest = max(longest, end - start + 1);
             } else {
@@ -60,4 +69,5 @@ int main () {
     cout << "\"abcabcbb\": exp. 3: " << S.lengthOfLongestSubstring("abcabcbb") << "\n";
     cout << "\"tmmzuxt\": exp. 5: " << S.lengthOfLongestSubstring("tmmzuxt") << "\n";
     cout << "\"\": exp. 0: " << S.lengthOfLongestSubstring("") << "\n";
+    cout << "\"aabaab!bb\": exp. 3: " << S.lengthOfLongestSubstring("aabaab!bb") << "\n";
 }
