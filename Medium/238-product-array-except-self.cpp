da4136aac5e3153a231c38cae700694f8ad61f65
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-02-27 13:30:54 -0500

    optimized solution for #238

--- Medium/238-product-array-except-self.cpp
+++ Medium/238-product-array-except-self.cpp
@@ -1,34 +1,33 @@
 #include <vector>
 #include <iostream>
 
-/* Runtime: 46 ms, faster than 26.17% of C++ online submissions for Product of Array Except Self.
-   Memory Usage: 26.3 MB, less than 6.60% of C++ online submissions for Product of Array Except Self.
+/* Runtime: 25 ms, faster than 75.53% of C++ online submissions for Product of Array Except Self.
+   Memory Usage: 24.9 MB, less than 36.28% of C++ online submissions for Product of Array Except Self.
 */
 
 /* Optimizations:
     Remove the prefix array
-    ???
+    Res becomes a modified version of suffixes
 */
 
 class Solution {
 public:
     std::vector<int> productExceptSelf(std::vector<int>& nums) {
-        std::vector <int> suffixes;
-        int suff = 1;
+        std::vector <int> res;
+        int pre = 1;
         
         int n = nums.size();
         
         for (int i = 0; i < n; i++) {
-            suffixes.push_back(suff);
-            suff = suff * nums[n - i - 1];
-        }
-        
-        std::vector <int> res;
-        int pre = 1;
-        
-        for (int i = 0; i < n; i++) {
-            res.push_back(pre * suffixes[n - i - 1]);
+            res.push_back(pre);
             pre = pre * nums[i];
+        }        
+        
+        int suff = 1;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = suff * res[i];
+            suff = suff * nums[i];
         }
         
         return res;
