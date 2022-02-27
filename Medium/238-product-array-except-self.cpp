55ff23b7de81c0bc1e4a3e68f5ba38e71550fc88
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-02-27 01:22:47 -0500

    added one optimization for #238

--- Medium/238-product-array-except-self.cpp
+++ Medium/238-product-array-except-self.cpp
@@ -1,28 +1,34 @@
 #include <vector>
 #include <iostream>
 
+/* Runtime: 46 ms, faster than 26.17% of C++ online submissions for Product of Array Except Self.
+   Memory Usage: 26.3 MB, less than 6.60% of C++ online submissions for Product of Array Except Self.
+*/
+
+/* Optimizations:
+    Remove the prefix array
+    ???
+*/
+
 class Solution {
 public:
     std::vector<int> productExceptSelf(std::vector<int>& nums) {
-        std::vector <int> prefixes;
         std::vector <int> suffixes;
-        int pre = 1;
         int suff = 1;
         
         int n = nums.size();
         
         for (int i = 0; i < n; i++) {
-            prefixes.push_back(pre);
-            pre = pre * nums[i];
-            
             suffixes.push_back(suff);
             suff = suff * nums[n - i - 1];
         }
         
         std::vector <int> res;
+        int pre = 1;
         
         for (int i = 0; i < n; i++) {
-            res.push_back(prefixes[i] * suffixes[n - i - 1]);
+            res.push_back(pre * suffixes[n - i - 1]);
+            pre = pre * nums[i];
         }
         
         return res;
