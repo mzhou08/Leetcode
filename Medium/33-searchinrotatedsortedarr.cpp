a49f093c57b03f855c98456b42f51300a4c74517
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-09-05 14:04:44 -0400

    added comments to 33

--- Medium/33-searchinrotatedsortedarr.cpp
+++ Medium/33-searchinrotatedsortedarr.cpp
@@ -6,23 +6,23 @@ public:
     int search(std::vector<int>& nums, int target) {
         int lo = 0;
         int hi = nums.size() - 1;
-        
+
+        // hi >= lo instead of hi != lo for singleton cases 
         while (hi >= lo) {
-            // std::cout << lo << " " << hi << "\n";
             
             int mid = ((hi - lo) / 2) + lo;
             
             if (nums[mid] == target) {
-                // std::cout << mid;
                 return mid;
             } else if (nums[mid] >= nums[lo]) {
+                // this means that the left side is sorted, had to change to >=
                 if (target < nums[mid] && target >= nums[lo]) {
-                    hi = mid - 1;
+                    hi = mid - 1;   // Since I did hi >= lo, we must never set hi or lo to mid
                 } else {
                     lo = mid + 1;
                 }
             } else {
-                // nums[mid] < nums[lo];
+                // nums[mid] < nums[lo]; so right side is sorted
                 if (target > nums[mid] && target <= nums[hi]) {
                     lo = mid + 1;
                 } else {
