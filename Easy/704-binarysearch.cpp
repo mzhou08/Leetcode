b8957e7dbaed6aa1f20ba64bd8fea7bb87735478
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-09-06 22:11:40 -0400

    cpp sol for 704: Binary Searcg

---
+++ Easy/704-binarysearch.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        
+        while (lo <= hi) {
+            int mid = (hi - lo) / 2 + lo;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+            
+        }
+        
+        return -1;
+        
+        
+    }
+};
