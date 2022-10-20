5ec333cde750dc4bb86fe2dda370082b2b808387
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-10-20 13:53:55 -0400

    CPP sol for #208: Implement Trie

---
+++ Medium/208-implementtrie.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class TrieNode {
+    public:
+    bool is_leaf;
+    vector<TrieNode*> next;
+
+    TrieNode() {
+        is_leaf = false;
+
+        next = vector<TrieNode*>(26);
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        // have to use iterative because the Trie stores TrieNode*
+
+        TrieNode* currNode = root;
+
+        for (char c: word) {
+            int char_num = int(c) - int('a');
+            if (!currNode->next[char_num]) {
+                currNode->next[char_num] = new TrieNode();
+            }
+            currNode = currNode->next[char_num];
+        }
+
+        currNode->is_leaf = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* currNode = root;
+
+        int char_num;
+        for (char c: word) {
+            char_num = int(c) - int('a');
+            if (currNode->next[char_num] == nullptr) {
+                return false;
+            }
+            currNode = currNode->next[char_num];
+        }
+
+        return (currNode->is_leaf);
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* currNode = root;
+
+        int char_num;
+        for (char c: prefix) {
+            char_num = int(c) - int('a');
+            if (currNode->next[char_num] == nullptr) {
+                return false;
+            }
+            currNode = currNode->next[char_num];
+        }
+
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
