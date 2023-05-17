82265075614acdc7254fa848d7483e818d86f687
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-05-16 19:03:34 -0700

    Divide & Conquer CPP solution for 973 - K Closest Points to Origin

--- Medium/973-kclosestpoints.cpp
+++ Medium/973-kclosestpoints.cpp
@@ -1,58 +1,41 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <random>
 
 using namespace std;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 
 class Solution {
+private:
+    double dist(vi point) {
+        return sqrt(point[0] * point[0] + point[1] * point[1]);
+    }
+
 public:
     vvi kClosest(vvi& points, int k) {
-        if (points.size() <= 1) {
-            return points;
-        }
+        if (points.size() <= k) return points;
+
+        int pivot = rand() % points.size();
+
+        vvi L; vvi R;
         
-        
-        int half = (points.size() / 2);
-        vvi left = vvi(points.begin(), points.begin() + half);
-        vvi right = vvi(points.begin() + half, points.end());
-        
-        
-        vvi leftK = kClosest(left, k);
-        vvi rightK = kClosest(right, k);
-        
-        vvi res = vvi(0, vi(0));
-        
-        for (int i = 0; i < k; i++) {
-            bool leftEmpty = (leftK.size() == 0);
-            bool rightEmpty = (rightK.size() == 0);
-            
-            if (leftEmpty && rightEmpty) {
-                return res;
-            } else if (leftEmpty) {
-                res.push_back(rightK[0]);
-                rightK.erase(rightK.begin());
-            } else if (rightEmpty) {
-                res.push_back(leftK[0]);
-                leftK.erase(leftK.begin());
+        for (int i = 0; i < points.size(); i++) {
+            if (dist(points[i]) <= dist(points[pivot])) {
+                L.push_back(points[i]);
             } else {
-                int leftDist = (
-                    pow(leftK[0][0], 2) + pow(leftK[0][1], 2)
-                );
-                int rightDist = (
-                    pow(rightK[0][0], 2) + pow(rightK[0][1], 2)
-                );
-                if (leftDist <= rightDist) {
-                    res.push_back(leftK[0]);
-                    leftK.erase(leftK.begin());
-                } else {
-                    res.push_back(rightK[0]);
-                    rightK.erase(rightK.begin());
-                }
+                R.push_back(points[i]);
             }
         }
-        
-        return res;
+
+        if (L.size() >= k) {
+            return kClosest(L, k);
+        } else {
+            vvi right = kClosest(R, k - L.size());
+            vvi res = right; res.reserve(right.size() + L.size());
+            res.insert(res.end(), L.begin(), L.end());
+            return res;
+        }
     }
 };
