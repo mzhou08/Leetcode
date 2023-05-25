0ba9b234109086ff9fe90f968203ae6e83e823f9
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-05-24 18:39:55 -0700

    optimized memory for 5 - Longest Palindrome. No longer DP, just looks back at previous

--- Medium/5-longestpalindrome.cpp
+++ Medium/5-longestpalindrome.cpp
@@ -14,8 +14,7 @@ public:
 
         if (s.size() == 0) return "";
 
-        vector<string> dp(s.size(), "");
-        dp[0] = s.substr(0, 1);
+        string res = s.substr(0, 1);
 
         // Thus, as we scan across the string at index i,
         // check if s[i] == s[i - dp[i-1] - 1]. If so,
@@ -23,35 +22,37 @@ public:
 
         for (int i = 1; i < s.size(); i++) {
             // default to previous
-            dp[i] = dp[i - 1];
+            string newLongest = res;
     
             // if adding s[i] made a new longer palindrome
             string palinPlusOne = s.substr(
-                i - dp[i - 1].size(), dp[i - 1].size() + 1
+                i - res.size(), res.size() + 1
             );
 
             if (isPalindrome(palinPlusOne)) {
-                dp[i] = palinPlusOne;
+                newLongest = palinPlusOne;
             }
 
 
             if (
-                dp[i - 1].size() < i
+                res.size() < i
                 ) {
                 // the last longest substring is not the whole previous string
 
                 // test palindrome plus one on each end
                 string palinPlusTwo = s.substr(
-                    i - dp[i - 1].size() - 1, dp[i - 1].size() + 2
+                    i - res.size() - 1, res.size() + 2
                 );
 
                 if (isPalindrome(palinPlusTwo)) {
-                    dp[i] = palinPlusTwo;
+                    newLongest = palinPlusTwo;
                 }
             }
+
+            res = newLongest;
             
         }
 
-        return dp[s.size() - 1];
+        return res;
     }
 };
