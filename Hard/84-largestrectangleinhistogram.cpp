e6180806738841f89b380b5f0fbe4c155cea5ec1
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-07-11 22:39:28 -0700

    CPP Stack solution for #84: Largest Rectangle in Histogram. omg.

--- Hard/84-largestrectangleinhistogram.cpp
+++ Hard/84-largestrectangleinhistogram.cpp
@@ -43,7 +43,74 @@ public:
 
 
             So we split on shortest bar.
+
+            But this O(nlogn) solution is too slow
         */
-        return areaHelper(heights, 0, heights.size() - 1);
+        // return areaHelper(heights, 0, heights.size() - 1);
+
+
+        /*
+            Need O(n) solution.
+
+            Maintain a stack of idx.
+
+            So let's traverse the heights from L to R.
+            At each new step, if it is a drop, then we
+            look at all previous heights that are at least as tall.
+
+            Pop off all pairs with height at least curr,
+            and consider curr * (1 + sum(widths)) as potential max.
+
+            Else if the heights is increasing, then we consider the
+            single bar a potential max, as well as
+            previous rectangle + 1 width.
+        */
+
+        vector<int> stack = {-1};
+
+        int max_area = 0;
+
+        for (int i = 0; i < heights.size(); i++) {
+            // if there is a decrease in height
+            while (
+                stack.back() != -1 &&
+                heights[stack.back()] >= heights[i]
+            ) {
+                // pop all entries on the stack taller than i
+                // why? all rectangles going through i
+                // will now be limited by i at least.
+                int limitingHeight = heights[stack.back()];
+                stack.pop_back();
+
+                int left = stack.back();
+
+                int right = i;
+
+                max_area = max(
+                    max_area,
+                    limitingHeight * (right - left - 1)
+                );
+            }
+
+            stack.push_back(i);
+        }
+
+        int n = heights.size();
+
+        // done with creating the stack
+        while (stack.back() != -1) {
+            int limitingHeight = heights[stack.back()];
+            stack.pop_back();
+
+            int left = stack.back();
+
+            max_area = max(
+                max_area,
+                limitingHeight * (n - left - 1)
+            );
+        }
+
+
+        return max_area;
     }
 };
