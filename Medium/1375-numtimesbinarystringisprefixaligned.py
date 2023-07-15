1399616a369ea674f58665649f62b25800c0565e
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-07-15 16:42:03 -0700

    Python solution for 13775 - Number of Times Binary String is Prefix-Aligned

---
+++ Medium/1375-numtimesbinarystringisprefixaligned.py
@@ -0,0 +1,42 @@
+class Solution:
+    def numTimesAllBlue(self, flips: List[int]) -> int:
+        """
+        Things to notice:
+        1-indexed.
+
+        Each bit is flipped only once.
+        So we start with 00...00
+        End with 11...11
+
+        So the question becomes, for how many bits do we have
+        flips[:i] == [1, 2, 3, 4 ... i - 1]?
+
+        We can do this by sorting by flip time.
+
+        At each step, just need to count the maximum
+        idx of a bit that has been flipped.
+
+        if it's == i-1 when we see position i, we know that it
+        is prefix-aligned.
+        """
+
+        maxBit = 0
+
+        flipTimes = sorted(
+            enumerate(flips),           # (i, time)
+            key=lambda x: x[1]
+        )
+
+        res = 0
+
+        print(flipTimes)
+
+        for idx, step in flipTimes:
+            maxBit = max(maxBit, idx)
+
+            if (maxBit + 1 == step):
+                # max bit set so far (1-indexed) is number of
+                # timesteps taken (0-indexed)
+                res += 1
+            
+        return res
